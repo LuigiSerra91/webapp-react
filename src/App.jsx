1e31a3fa84ee2eadd8dedea8c2a6adadeb0cf148
LuigiSerra91 <luigi_serra91@hotmail.it>
2024-12-11 14:06:45 +0100

    add_components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from "react-router"
+import DefaultLayout from "./layout/DefaultLayout.jsx"
 
 function App() {
 
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index path="/" element={<h1>Hello</h1>}></Route>
+            <Route path="users" element={<h2>User List</h2>}></Route>
 
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom"
+import Header from "../components/Header.jsx"
+import Footer from "../components/Footer.jsx"
 export default function DefaultLayout() {
     return (
         <>
-            <header>Welcome to Layout</header>
-            <main><Outlet /></main>
-            <footer> welcome to footer</footer>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+
 
         </>
     )
